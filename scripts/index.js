22a491e00e0ce710c2110c3933a301b413108aa9
AliHakim773 <alihakim7732@gmail.com>
2023-11-14 21:53:24 +0200

    added typing animations

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+// Typing Animation
+const dynamic_text = document.getElementById("hero-left-title-change")
+const hero_left_container = document.getElementById("hero-left-container")
+const hero_left_description = document.getElementById("hero-left-description")
+const words = ["Sofware Engineer?", "Data Engineer?", "UI/UX Designer"]
+const bg_colors = ["#00eea7ed", "#9864daed", "#fb508eed"]
+
+let word_index = 0
+let char_index = 2
+let is_deleting = false
+
+const typeEffect = () => {
+    const current_word = words[word_index]
+    const current_bg_color = bg_colors[word_index]
+    const current_color = word_index == 0 ? "var(--black)" : "var(--white)"
+    const current_char = current_word.substring(0, char_index)
+    dynamic_text.textContent = current_char
+    dynamic_text.classList.add("stop-blinking")
+    dynamic_text.style.color = current_color
+    hero_left_container.style.backgroundColor = current_bg_color
+    if (word_index == 0) {
+        dynamic_text.classList.add("color_black")
+    } else {
+        dynamic_text.classList.remove("color_black")
+    }
+
+    if (!is_deleting && char_index < current_word.length) {
+        char_index++
+        setTimeout(typeEffect, 100)
+    } else if (is_deleting && char_index > 0) {
+        char_index--
+        setTimeout(typeEffect, 50)
+    } else {
+        is_deleting = !is_deleting
+        dynamic_text.classList.remove("stop-blinking")
+        word_index = !is_deleting ? (word_index + 1) % words.length : word_index
+        setTimeout(typeEffect, 1000)
+    }
+}
+typeEffect()
