c913c1ed5028df77c12bcdd0f1781c7f24acbebb
AliHakim773 <alihakim7732@gmail.com>
2023-11-15 23:06:05 +0200

    added carousel + responsiveness

--- scripts/index.js
+++ scripts/index.js
@@ -72,3 +72,59 @@ tabs.forEach((e, i) => {
         tabs_body[i].classList.remove("d-none")
     })
 })
+
+// carousel
+let slideIndex = 1
+let myTimer
+let slideshowContainer
+const delay = 5000
+
+window.addEventListener("load", function () {
+    showSlides(slideIndex)
+    myTimer = setInterval(function () {
+        plusSlides(1)
+    }, delay)
+})
+function plusSlides(n) {
+    clearInterval(myTimer)
+    if (n < 0) {
+        showSlides((slideIndex -= 1))
+    } else {
+        showSlides((slideIndex += 1))
+    }
+    if (n === -1) {
+        myTimer = setInterval(function () {
+            plusSlides(n + 2)
+        }, delay)
+    } else {
+        myTimer = setInterval(function () {
+            plusSlides(n + 1)
+        }, delay)
+    }
+}
+function currentSlide(n) {
+    clearInterval(myTimer)
+    myTimer = setInterval(function () {
+        plusSlides(n + 1)
+    }, delay)
+    showSlides((slideIndex = n))
+}
+function showSlides(n) {
+    let i
+    const slides = document.getElementsByClassName("carousel-slide")
+    const dots = document.getElementsByClassName("carousel-indicator")
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    slides[slideIndex - 1].style.display = "block"
+    dots[slideIndex - 1].className += " active"
+}
